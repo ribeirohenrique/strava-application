adc4a1a2c893f9beae6f42732ebe6814737f6cd8
Henrique Mendes <henrique1258@gmail.com>
2023-04-11 17:55:06 +0000

    Chamada Funcional da API do strava usando fetch

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response, response } from "express";
+import dotenv from "dotenv";
+import { ResponseDto } from "./dto/ResponseDto";
 
 dotenv.config();
 
-const app: Express = express();
+const router = express();
 const port = process.env.PORT;
+const token = process.env.TOKEN;
+const profile_link = `https://www.strava.com/api/v3/athlete?access_token=${token}`;
+console.log(profile_link);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+router.get("/", async (req: Request, res: Response): Promise<ResponseDto> => {
+  const options: RequestInit = {
+    method: "GET",
+  };
+
+  fetch(profile_link, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+  try {
+    let responseFetch = await fetch(profile_link, options);
+    let response = (await responseFetch.json()) as ResponseDto;
+    res.status(200).json(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: `Internal Server Error.` });
+  }
 });
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+router.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
---
+++ src/dto/ResponseDto.ts
@@ -0,0 +1,22 @@
+  export interface ResponseDto {
+  id: number,
+  username: string,
+  resource_state: number,
+  firstname: string,
+  lastname: string,
+  bio: string,
+  city: string,
+  state: string,
+  country: string,
+  sex: string,
+  premium: boolean,
+  summit: boolean,
+  created_at: string,
+  updated_at: string,
+  badge_type_id: number,
+  weight: number,
+  profile_medium: string,
+  profile: string,
+  friend: any,
+  follower: any
+  }
